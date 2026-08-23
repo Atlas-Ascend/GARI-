03eb1ad54d2c67274376a2566df8c1633a0dcd4c
Atlas-Ascend <ghostatlasempire@gmail.com>
2026-08-23 16:28:35 -0700

    Add Homeward Unreal module rules

---
+++ games/homeward-unreal/Source/Homeward/Homeward.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class Homeward : ModuleRules
+{
+    public Homeward(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        PublicDependencyModuleNames.AddRange(new string[] {
+            "Core", "CoreUObject", "Engine", "InputCore"
+        });
+    }
+}
